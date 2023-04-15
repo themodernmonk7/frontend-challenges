276ca53313a749b7985ef39528c7e077ef4b9885
themodernmonk7 <sauravavishek@gmail.com>
2023-04-15 20:40:33 +0530

    file setup

---
+++ 02. OTP Input/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <h1 className=" my-28 bg-yellow-400 py-2 text-center text-3xl ">
+      Vite + React + TailwindCSS template
+    </h1>
+  )
+}
+
+export default App
