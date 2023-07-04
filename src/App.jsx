c5fd1dc8a42b25587d2316f905bd701e7153e254
themodernmonk7 <sauravavishek@gmail.com>
2023-07-04 14:25:12 +0530

    setup file

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 function App() {
   return (
-    <h1 className=" my-28 bg-yellow-400 py-2 text-center text-3xl ">
-      Vite + React + TailwindCSS template
+    <h1>
+      Challenge
     </h1>
   )
 }
