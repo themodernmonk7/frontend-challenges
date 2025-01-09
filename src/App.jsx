ecf03f7ef4b004be9ffc61aa2ec13ece2ee90922
themodernmonk7 <sauravavishek@gmail.com>
2025-01-09 18:47:58 +0530

    complete upi-autocomplete

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,101 @@
+import { useState } from "react"
+import { BANK_UPI_HANDLES, GRAPHIC_URL } from "./constants/constants"
+
 function App() {
-  return <h1>Challenge</h1>
+  const [input, setInput] = useState("")
+  const [predictValue, setPredictValue] = useState("")
+  const [predictions, setPredictions] = useState([])
+
+  const handleChange = (e) => {
+    const {
+      target: { value },
+    } = e
+    setInput(value.toLowerCase())
+
+    if (!value || !value.includes("@")) {
+      setPredictValue(value || "")
+      setPredictions([])
+      return
+    }
+    const [userVPA, userBankName] = value.split("@")
+
+    if (!userVPA) return
+
+    const userBankRegex = new RegExp(`${userBankName}`)
+    const matchedBankNames = BANK_UPI_HANDLES.filter((bank) => {
+      return userBankRegex.test(bank)
+    })
+    let currentPredictedBankName = matchedBankNames[0]
+    if (userBankName && !matchedBankNames.length) {
+      currentPredictedBankName = ""
+    }
+    setPredictValue(`${userVPA}@${currentPredictedBankName}`)
+    setPredictions(matchedBankNames)
+  }
+
+  const handleKeyPressDown = (e) => {
+    const { which = -1, keyCode = -1, code = "" } = e
+    const isRightArrowKey =
+      which === 39 || keyCode === 39 || code.toLowerCase() === "arrowrigh"
+    const isTabKey =
+      which === 9 || keyCode === 9 || code.toLowerCase() === "tab"
+    if (isRightArrowKey || isTabKey) {
+      setInput(predictValue)
+      setPredictions([])
+    }
+  }
+
+  const handleBankNameClicked = (e) => {
+    const { target } = e
+    const currentBankName = target.getAttribute("data-bank-name")
+    const [currentUserVPA] = input.split("@")
+    const updatedUpiId = `${currentUserVPA}@${currentBankName}`
+
+    setInput(updatedUpiId)
+    setPredictValue(updatedUpiId)
+    setPredictions([])
+  }
+
+  return (
+    <section className=" m-12 flex flex-col items-center justify-center space-y-4 rounded-md border border-green-400 p-4">
+      <img src={GRAPHIC_URL} alt="payment upi" />
+      <div className="relative">
+        <input
+          className="focus:none absolute left-0 top-0 -z-10 w-full border-b border-l-0 border-r-0 border-t-0 border-gray-400 lowercase opacity-50 focus:ring-0"
+          type="text"
+          value={predictValue}
+          onChange={() => {}}
+        />
+        <input
+          value={input}
+          onChange={(e) => handleChange(e)}
+          type="text"
+          placeholder="Enter your UPI ID"
+          className="focus:none z-10 w-full border-b border-l-0 border-r-0 border-t-0  border-gray-400 bg-transparent lowercase placeholder:text-xs placeholder:capitalize placeholder:text-gray-400 focus:ring-0"
+          onKeyDown={handleKeyPressDown}
+        />
+        {predictions.length ? (
+          <ul className="bottom-0-0 absolute z-30 flex max-h-40 w-full flex-col overflow-y-scroll border bg-white">
+            {predictions.map((handle) => {
+              return (
+                <button
+                  data-bank-name={handle}
+                  onClick={(e) => handleBankNameClicked(e)}
+                  key={handle}
+                  className="border-b py-1 text-sm"
+                >
+                  {handle}
+                </button>
+              )
+            })}
+          </ul>
+        ) : null}
+      </div>
+      <button className="w-full rounded-md bg-green-400 py-1 text-white shadow-md">
+        Pay Now
+      </button>
+    </section>
+  )
 }
 
 export default App
---
+++ src/constants/constants.js
@@ -0,0 +1,21 @@
+export const BANK_UPI_HANDLES = [
+  "oksbi", // State Bank of India
+  "okicici", // ICICI Bank
+  "okhdfcbank", // HDFC Bank
+  "okaxisbank", // Axis Bank
+  "okpnb", // Punjab National Bank
+  "okdbs", // DBS Bank
+  "okubib", // Union Bank of India
+  "okbankofbaroda", // Bank of Baroda
+  "okcanarabank", // Canara Bank
+  "okindusind", // IndusInd Bank
+  "okkotak", // Kotak Mahindra Bank
+  "oksouthindian", // South Indian Bank
+  "okfederalbank", // Federal Bank
+  "okkarurvysya", // Karur Vysya Bank
+  "oksaraswat", // Saraswat Bank
+  "okjkb", // Jammu & Kashmir Bank
+]
+
+export const GRAPHIC_URL =
+  "https://uploads.codesandbox.io/uploads/user/db72b415-e33f-48d9-ba47-160088dc6c79/78ab-payment-graphic.png"
